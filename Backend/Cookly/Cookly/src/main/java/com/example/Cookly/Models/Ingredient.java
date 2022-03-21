3d6ece71809fab459e4f5589efbe8a41a7fee64c
Michal Babinski <babinmichal@gmail.com>
2022-03-21 18:01:53 +0400

    added getters and setters in models

--- Backend/Cookly/Cookly/src/main/java/com/example/Cookly/Models/Ingredient.java
+++ Backend/Cookly/Cookly/src/main/java/com/example/Cookly/Models/Ingredient.java
@@ -1,7 +1,19 @@
+@Entity
+@Table(name="Ingredients")
 public class Ingredient{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @ManyToMany(mappedBy = "ingredientsUsedInRecipe")
+    Set<Recipe> recipes;
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
 
     @Column(nullable = false) private String name;
 
