389c0f71314689502ee76164b3c57f4341a11246
giermczak <wojtek.buchwald@windowslive.com>
2022-03-22 14:59:13 +0100

    [FIX] model changes

--- Backend/Cookly/src/main/java/com/example/Cookly/Models/Recipe.java
+++ Backend/Cookly/src/main/java/com/example/Cookly/Models/Recipe.java
@@ -1,8 +1,79 @@
 package com.example.Cookly.Models;
 
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
 
-@Entity(name = "recipes")
+
+@Entity
+@Table(name="Recipes")
 public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(nullable = false) private String name;
+    @Column(nullable = false) private String instructions;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public Set<User> getUsersWhoLiked() {
+        return usersWhoLiked;
+    }
+
+    public void setUsersWhoLiked(Set<User> usersWhoLiked) {
+        this.usersWhoLiked = usersWhoLiked;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public Set<Ingredient> getIngredientsUsedInRecipe() {
+        return ingredientsUsedInRecipe;
+    }
+
+    public void setIngredientsUsedInRecipe(Set<Ingredient> ingredientsUsedInRecipe) {
+        this.ingredientsUsedInRecipe = ingredientsUsedInRecipe;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    @ManyToMany
+    @JoinTable(
+            name = "ingredients_used_in_recipe",
+            joinColumns = @JoinColumn(name = "id"),//recipe id
+            inverseJoinColumns = @JoinColumn(name = "id"))//ingredient id
+    Set<Ingredient> ingredientsUsedInRecipe;
+
+    @ManyToMany(mappedBy = "likedRecipes")
+    Set<User> usersWhoLiked;
+
+    @Column(nullable = false, name = "tag")
+    private List<String> tags;
+
 }
