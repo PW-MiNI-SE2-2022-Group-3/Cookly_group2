b2cb0773e47d60f82f5e3774de49bd13024a8529
Wojtek Buchwald <wojtek.buchwald@windowslive.com>
2022-04-02 22:52:32 +0200

    [ADD] Rest ingredient modem mapper

--- Backend/Cookly/src/main/java/com/example/Cookly/mapper/IngredientMapper.java
+++ Backend/Cookly/src/main/java/com/example/Cookly/mapper/IngredientMapper.java
@@ -2,6 +2,7 @@ package com.example.Cookly.mapper;
 
 import com.example.Cookly.DTOModels.IngredientDTO;
 import com.example.Cookly.business.ingredient.model.Ingredient;
+import com.example.Cookly.restModels.IngredientRest;
 
 import java.util.Objects;
 import java.util.Optional;
@@ -35,6 +36,26 @@ public class IngredientMapper {
             ingredient.setName(ingredientDTO.getName());
             ingredient.setQuantity(quantity);
             return Optional.of(ingredient);
+        }
+        return Optional.empty();
+    }
+
+    public static Optional<Ingredient> mapToIngredient(final IngredientRest ingredientRest) {
+        if (Objects.nonNull(ingredientRest)) {
+            final Ingredient ingredient = new Ingredient();
+            ingredient.setIngredientId(ingredientRest.getId());
+            ingredient.setName(ingredientRest.getName());
+            return Optional.of(ingredient);
+        }
+        return Optional.empty();
+    }
+
+    public static Optional<IngredientRest> mapToIngredientRest(final Ingredient ingredient) {
+        if (Objects.nonNull(ingredient)) {
+            IngredientRest ingredientRest = new IngredientRest();
+            ingredientRest.setId(ingredient.getIngredientId());
+            ingredientRest.setName(ingredient.getName());
+            return Optional.of(ingredientRest);
         }
         return Optional.empty();
     }
