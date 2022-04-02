ff27d7b64057dafaa464ac1a23f573571e7676c6
Wojtek Buchwald <wojtek.buchwald@windowslive.com>
2022-04-02 23:48:11 +0200

    [REF] Old controller remove

--- Backend/Cookly/src/main/java/com/example/Cookly/Controllers/IngredientController.java
+++ Backend/Cookly/src/main/java/com/example/Cookly/Controllers/IngredientController.java
@@ -1,18 +1,4 @@
 package com.example.Cookly.Controllers;
 
-/*
-@RestController
-@RequestMapping("/ingredients")
 public class IngredientController {
-
-    @Autowired
-    private IngredientRepository ingredientRepository;
-
-
-
-
-
 }
-
-
- */
