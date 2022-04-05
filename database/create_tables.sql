243a9db52e8fd536dd07eaf290e319962bafc2ec
Wojtek Buchwald <wojtek.buchwald@windowslive.com>
2022-04-05 02:27:57 +0200

    [ADD] mysql engine

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -11,10 +11,10 @@ drop table tags;
 drop table users;
 
 create table ingredients (
-    id int AUTO_INCREMENT,
+    id int not null AUTO_INCREMENT,
     name varchar(30) not null,
     constraint ingredients_pk primary key (id)
-)AUTO_INCREMENT = 1;
+) engine = INNODB;
 
 
 
@@ -23,70 +23,70 @@ create table recipes (
     name varchar(30) not null,
     instructions varchar(1000) not null,
     constraint recipes_pk primary key (id)
-);
+)engine = INNODB;
 
 create table shops (
     id int NOT NULL AUTO_INCREMENT,
     name varchar(30) not null,
     constraint shops_pk primary key (id)
-);
+)engine = INNODB;
 
 create table restaurants (
     id int NOT NULL AUTO_INCREMENT,
     name varchar(30) not null,
     constraint restaurants_pk primary key (id)
-);
+)engine = INNODB;
 
 create table tags (
     id int NOT NULL AUTO_INCREMENT,
     name varchar(30) not null,
     constraint tags_pk primary key (id)
-);
+)engine = INNODB;
 
 create table users (
     id int NOT NULL AUTO_INCREMENT,
     username varchar(30) not null,
     password varchar(64) not null,
     token varchar(36) not null,
-    is_admin number not null,
+    is_admin int not null,
     constraint users_pk primary key (id)
-);
+)engine = INNODB;
 
 create table ingredient_recipe (
-    recipe_id number not null,
-    ingredient_id number not null,
+    recipe_id int not null,
+    ingredient_id int not null,
     quantity varchar(30),
     constraint recipes_ingredient_fk foreign key (recipe_id) references recipes(id),
     constraint ingredient_recipe_fk foreign key (ingredient_id) references ingredients(id)
 );
 
 create table restaurant_recipe (
-    recipe_id number not null,
-    restaurant_id number not null,
-    price number not null,
+    recipe_id int not null,
+    restaurant_id int not null,
+    price int not null,
     constraint recipes_restaurants_fk foreign key (recipe_id) references recipes(id),
     constraint restaurants_recipes_fk foreign key (restaurant_id) references restaurants(id)
 );
 
 create table shop_ingredient (
-    shop_id number not null,
-    ingredient_id number not null,
-    price number not null,
+    shop_id int not null,
+    ingredient_id int not null,
+    price int not null,
     constraint shops_ingredients_fk foreign key (shop_id) references shops(id),
     constraint ingredients_shops_fk foreign key (ingredient_id) references ingredients(id)
 );
 
 
 create table recipe_user (
-    recipe_id number not null,
-    user_id number not null,
+    recipe_id int not null,
+    user_id int not null,
     constraint recipes_users_fk foreign key (recipe_id) references recipes(id),
     constraint users_recipes_fk foreign key (user_id) references users(id)
 );
 
 create table recipe_tag (
-    recipe_id number not null,
-    tag_id number not null,
+    recipe_id int not null,
+    tag_id int not null,
     constraint recipes_tagss_fk foreign key (recipe_id) references recipes(id),
     constraint tags_recipes_fk foreign key (tag_id) references tags(id)
 );
