b03e3d71280a77274f4be7e7ad317347d582e526
giermczak <wojtek.buchwald@windowslive.com>
2022-04-07 17:33:37 +0200

    [ADD] undelete mock test

--- Backend/Cookly/src/test/java/com/example/cookly/CooklyApplicationTests.java
+++ Backend/Cookly/src/test/java/com/example/cookly/CooklyApplicationTests.java
@@ -4,12 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CooklyApplicationTests {
 
-	//@Test
-	//void contextLoads() {
-	//	Assertions.assertTrue(1 == 1);
-	//}
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(1 == 1);
+	}
 
 }
